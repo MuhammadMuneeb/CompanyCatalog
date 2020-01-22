4b13101867aad4dec1c5256a28c85ae8240dd9f1
MuhammadMuneeb <haqgreat@gmail.com>
2020-01-22 10:36:56 +0500

    Added the category model

--- app/Category.php
+++ app/Category.php
@@ -2,9 +2,45 @@
 
 namespace App;
 
+use App\CommonUtils\CommonUtils;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $primaryKey = 'id';
+    protected $fillable = [
+    	'category'
+    ];
+    
+    public static function allCategory(){
+    	$all = self::all();
+    	return $all;
+    }
+    
+    public static function getCategory($id){
+    	$cat = self::where('id', $id)->first();
+    	return $cat;
+    }
+    
+    public static function addCategory($data){
+    	$cat = new self;
+    	$all_cat = self::allCategory();
+    	foreach($all_cat as $cats){
+    	  if(strcasecmp($cats->category, $data) == 0){
+    	  	return 'Already exists';
+	      }
+	    }
+    	$cat->category = $data;
+    	$result = $cat->save();
+    	
+    	return CommonUtils::returnString($result);
+    }
+    
+    public static function deleteCategory($id){
+    	$cat = self::find($id);
+    	$result = $cat->delete();
+    	return CommonUtils::returnString($result);
+    }
+    
+    
 }
