1ad2d23e6c51f0d9b1bb93ec2228bf31c1a8764e
MuhammadMuneeb <haqgreat@gmail.com>
2020-01-29 05:31:19 +0500

    CompanyController works for saving company

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Company;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
@@ -16,15 +18,18 @@ class CompanyController extends Controller
     
     public function addCompanyPage(){
     	try{
-    	  return view('layouts.company.add_company');
+    		$categories = Category::allCategory();
+    	  return view('layouts.company.add_company')->with(compact('categories'));
 	    }catch(\Exception $exception){
     		return $exception->getMessage().' '.$exception->getLine();
 	    }
 		}
 		
-		public function addCompany(Request $request){
+		public function saveCompany(Request $request){
     	try{
-    	
+    		$data = $request->all();
+    		$result = Company::addUpdateCompany($data);
+    		return $result;
 	    }catch(\Exception $exception){
     		return $exception->getMessage().' '.$exception->getLine();
 	    }
@@ -35,6 +40,14 @@ class CompanyController extends Controller
     	
 	    }catch(\Exception $exception){
     		return $exception->getMessage().' '.$exception->getLine();
+	    }
+		}
+		
+		public function addCompanyCatPage(){
+    	try{
+    		return view('layouts.company.select_categories');
+	    }catch (\Exception $exception){
+    		return $exception->getMessage();
 	    }
 		}
 		
