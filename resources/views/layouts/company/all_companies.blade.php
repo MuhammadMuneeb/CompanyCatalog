b99de14dd3acc39a3ee3ff8eca90a451efdfc0d3
MuhammadMuneeb <haqgreat@gmail.com>
2020-01-30 12:42:18 +0500

    Added page for showing all companies

--- resources/views/layouts/company/all_companies.blade.php
+++ resources/views/layouts/company/all_companies.blade.php
@@ -1,5 +1,66 @@
 @extends('layouts.master')
 
 @section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>All Companies</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Companies</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
 
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+                <div class="col-12">
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Company data with keywords and descriptions</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="companies_table" class="table table-bordered table-striped">
+                                <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Website</th>
+                                    <th>Keywords</th>
+                                    <th>Description</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($companies as $company)
+                                <tr>
+                                    <td>{{$company->name}}</td>
+                                    <td><a href="{{$company->website}}">{{$company->website}}</a></td>
+                                    <td>{{$company->keywords}}</td>
+                                    <td>{{$company->description}}</td>
+                                    <td>Buttons</td>
+                                </tr>
+                                @endforeach
+
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </section>
+        <!-- /.content -->
+    </div>
 @endsection
